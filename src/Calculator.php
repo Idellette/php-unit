a80fbb920fe53e13bc2a47893dbeeea70f47075a
Idellette <idellettesom@yahoo.fr>
2024-07-10 19:43:13 +0200

    challenge commit

--- src/Calculator.php
+++ src/Calculator.php
@@ -8,4 +8,20 @@ class Calculator
     {
         return $firstValue + $secondValue;
     }
+// second Test
+    public function multiplication(int $firstValue, int $secondValue ): int 
+    {
+        return $firstValue * $secondValue;
+    }
+// third test
+    public function isEven(int $number): bool 
+    {
+       if($number % 2 === 0){
+        return true ;
+       }
+       else{
+        return false ;
+       }
+        
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,4 +22,35 @@ class CalculatorTest extends TestCase
             "La méthode sum() ne fonctionne plus :-("
         );
     }
+// second test
+    public function testMultiplication(): void
+    {
+        //Arrange
+        $firstValue = 5;
+        $secondValue = 8;
+        $expectedResult = 40;
+
+        //Act
+        $calculator = new Calculator();
+        $multiplication= $calculator->multiplication($firstValue, $secondValue);
+        
+        //Assert
+        $this->assertSame($expectedResult, $multiplication, 
+            "La méthode multiplication() ne fonctionne plus :-("
+        );
+    }
+    // third  test
+    public function testIsEven(): void
+    {
+        //Arrange
+        $number = 10;
+        $expectedResult = true;
+
+        //Act
+        $calculator = new Calculator();
+        $isEven = $calculator->isEven($number);
+        
+        //Assert
+        $this->assertTrue(true);        
+    }
 }
